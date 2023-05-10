TheNoviceProspect/SimpleDiscordBot

commit 9d58c8fe89995aea8b1764097f3cfe98b8723f45

Implemented /weather command
- WeatherData.cs comes directly from pasting JSON into Visual Studio
- Replaced Newtonsoft Json with M$ implementation
- Fixed JSON deserializer
- Code cleanup to remove a lot of debugging output

app/services/WeatherService.cs
@@ -1,23 +1,14 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using sdb_app.services.data;
+using System.Text.Json;
 
 namespace sdb_app.services;
 
-/// <summary>
-/// This is a container for Weather Data
-/// </summary>
-public class WeatherData
-    {
-        public string? Temperature { get; set; }
-        public string? Humidity { get; set; }
-        public string? WindSpeed { get; set; }
-    }
 
 /// <summary>
 /// The Weather Service class
@@ -92,96 +83,51 @@ public class WeatherService {
     /// </summary>
     /// <param name="_city">Any city name should do, if none is given London, UK is assumed</param>
     /// <returns>an asynchronous result containing <see>WeatherData</see></returns>
-    public async Task<String> GetWeather(string _city = "London", string _country = "UK") {
+    public async Task<WeatherData> GetWeather(WeatherService.WeatherEndpoint _endpoint, string _city = "London", string _country = "UK") {
         // Get the API key from Weather Underground.
         string apiKey = sdb_app.Program.WeatherToken;
 
         // Build the query string depending on wether the endpoint supports countries or not.
         string _queryString = String.Empty;
-        string _debugQueryString = String.Empty;
-        if (MyEndpoint.CountryQueryString=="")
-        {
-            _queryString = $"{MyEndpoint.Endpoint}{MyEndpoint.CityQueryString}{_city}&{MyEndpoint.AppID}{apiKey}";
-            // make sure there is key when we say "redacted" or "empty" when it is
-            if (apiKey==String.Empty)
-            {
-                _debugQueryString = $"{MyEndpoint.Endpoint}{MyEndpoint.CityQueryString}{_city}&{MyEndpoint.AppID}[EMPTY]";
-            } else
-            {
-                _debugQueryString = $"{MyEndpoint.Endpoint}{MyEndpoint.CityQueryString}{_city}&{MyEndpoint.AppID}[REDACTED]";
-            }
-        
-        } else
-        {
-            _queryString = $"{MyEndpoint.Endpoint}{MyEndpoint.CityQueryString}{_city}&{MyEndpoint.CountryQueryString}{_country}&{MyEndpoint.AppID}{apiKey}";
-            // make sure there is key when we say "redacted" or "empty" when it is
-            if (apiKey == String.Empty)
-            {
-                _debugQueryString = $"{MyEndpoint.Endpoint}{MyEndpoint.CityQueryString}{_city}&{MyEndpoint.CountryQueryString}{_country}&{MyEndpoint.AppID}[EMPTY]";
-            }
-            else
-            {
-                _debugQueryString = $"{MyEndpoint.Endpoint}{MyEndpoint.CityQueryString}{_city}&{MyEndpoint.CountryQueryString}{_country}&{MyEndpoint.AppID}[REDACTED]";
-            }
-        }
-
-        // Log the entire query
-        if (sdb_app.Program.IsDebug)
-        {
-            Program.discordClient.Logger.Log(LogLevel.Information, $"This is the full query: {_debugQueryString}", MyEndpoint);
-        }
 
         // Create a new HttpClient object.
         HttpClient client = new HttpClient();
 
-        if (sdb_app.Program.IsDebug)
-        {
-            Program.discordClient.Logger.Log(LogLevel.Information, $"This is the HTTP client: {client.DefaultRequestHeaders}", client);
-        }
-
-        // Set the Content-Type header to application/x-www-form-urlencoded.
-        //client.DefaultRequestHeaders.Add("Content-Type", "application/x-www-form-urlencoded");
-        client.DefaultRequestHeaders.Add("Content-Type", "application/json");
-
-        if (sdb_app.Program.IsDebug)
-        {
-            Program.discordClient.Logger.Log(LogLevel.Information, $"Headers were changed to : {client.DefaultRequestHeaders.ToString}", client);
-        }
-
-        // Make the request to the Weather API.
-        //HttpResponseMessage response = await client.GetAsync(MyEndpoint.Endpoint + _queryString);
-        
         // Create the request before sending it to the server.
-        HttpRequestMessage request = new HttpRequestMessage(HttpMethod.Get, _queryString);
+        Uri request = new Uri(_queryString);
 
         if (sdb_app.Program.IsDebug)
         {
-            Program.discordClient.Logger.Log(LogLevel.Information, $"This is the api request: {request.ToString}", MyEndpoint);
+            Program.discordClient.Logger.Log(LogLevel.Information, $"This is the api request: {request.ToString}", _endpoint);
         }
 
         // Send the request
-        HttpResponseMessage response = await client.SendAsync(request);
-
+        HttpResponseMessage response = await client.GetAsync(request);
+        string rawWeatherData = String.Empty;
         // Check the response status code.
         if (response.StatusCode == HttpStatusCode.OK)
         {
             // Get the weather data from the response body.
-            string rawWeatherData = await response.Content.ReadAsStringAsync();
+            rawWeatherData = await response.Content.ReadAsStringAsync();
             if (sdb_app.Program.IsDebug)
             {
                 sdb_app.Program.discordClient.Logger.Log(LogLevel.Information, "We have received a response as follows", rawWeatherData);
             }
 
             // Parse the weather data into a Weather object.
-            // TODO(smzb): this needs addressed since the json response is more "layered"
-            //WeatherData? weather = JsonConvert.DeserializeObject<WeatherData>(rawWeatherData);
-
-            return rawWeatherData;
         }
         else
         {
             // Handle the error.
             throw new Exception("Error: " + response.StatusCode);
+        }
+        // Deserialize the raw json data against the WeatherData class object from the rawWeatherData into rawWeather
+        WeatherData? rawWeather = JsonSerializer.Deserialize<WeatherData>(rawWeatherData);
+        // Error handling, probably shoddy too..
+        if (rawWeather==null) {
+            throw new Exception("The JSON data could not be serialized!");
+        } else {
+            return rawWeather;
         }
     }
 

app/slashCommands/WeatherCommand.cs
@@ -4,6 +4,7 @@ using DSharpPlus.SlashCommands;
 using System.Threading.Tasks;
 using sdb_app.services;
 using Microsoft.Extensions.Logging;
+using sdb_app.services.data;
 
 namespace sdb_app.slashCommands;
 public class WeatherCommandModule : ApplicationCommandModule {
@@ -20,17 +21,13 @@ public class WeatherCommandModule : ApplicationCommandModule {
         WeatherService service = new WeatherService();
         // set its endpoint to that of weatherbit.io
         service.MyEndpoint = service.WeatherEndpoints.Find(x => x.Name.Equals("WeatherBit"));
-        // Now output the full query
-        content += "Querying " + service.MyEndpoint.Endpoint + service.MyEndpoint.CityQueryString + _city + "&" + service.MyEndpoint.CountryQueryString + _country + "&" + service.MyEndpoint.AppID + "*REDACTED*" + NEWLINE;
-        await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent(content));
-        // Get the weather for the location.
-        //WeatherData weather = await service.GetWeather(_city,_country);
-        string weather = await service.GetWeather(_city,_country);
+        // Get the weather for the location through our specific endpoint
+        WeatherData weather = await service.GetWeather(service.MyEndpoint, _city,_country);
+        // Simplify data access
+        var data = weather.data[0];
         // Display the weather information.
-        // content += "The current temperature in " + _city + " is " + weather.Temperature + NEWLINE;
-        // content += "The current humidity in " + _city + " is " + weather.Humidity + NEWLINE;
-        // content += "The current wind speed in " + _city + " is " + weather.WindSpeed + NEWLINE;
-        content += "This is the raw response : " + NEWLINE + "`"+weather+"`"+NEWLINE;
+        content += "The current temperature in " + data.city_name + "," + data.country_code + " is " + data.temp + "°C" + NEWLINE;
+        content += "The current humidity is " + data.rh + "% and the current wind speed is " + data.wind_spd + "m/s hailing from " + data.wind_cdir_full + NEWLINE;
         content += "~~~" + NEWLINE;
         content += "Thanks for waiting!";
         await ctx.EditResponseAsync(new DiscordWebhookBuilder().WithContent(content));

app/services/WeatherData.cs
@@ -1,0 +1,65 @@
+/// <summary>
+/// This is essentially a class representation of a Weatherbit.io json response
+/// Most of this is from https://www.weatherbit.io/api/weather-current
+/// </summary>
+
+namespace sdb_app.services.data {
+    public class WeatherData
+    {
+        public int? count { get; set; }
+        /// <summary>
+        /// In general one would only expect one item in this array
+        /// </summary>
+        /// <value>Always refer to data[0].PROPERTY</value>
+        public Datum[]? data { get; set; }
+    }
+
+    public class Datum
+    {
+        public float? app_temp { get; set; }
+        public int? aqi { get; set; }
+        public string? city_name { get; set; }
+        public int? clouds { get; set; }
+        public string? country_code { get; set; }
+        public string? datetime { get; set; }
+        public float? dewpt { get; set; }
+        public float? dhi { get; set; }
+        public float? dni { get; set; }
+        public float? elev_angle { get; set; }
+        public float? ghi { get; set; }
+        public float? gust { get; set; }
+        public float? h_angle { get; set; }
+        public float? lat { get; set; }
+        public float? lon { get; set; }
+        public string? ob_time { get; set; }
+        public string? pod { get; set; }
+        public float? precip { get; set; }
+        public float? pres { get; set; }
+        public int? rh { get; set; }
+        public float? slp { get; set; }
+        public int? snow { get; set; }
+        public float? solar_rad { get; set; }
+        public string[]? sources { get; set; }
+        public string? state_code { get; set; }
+        public string? station { get; set; }
+        public string? sunrise { get; set; }
+        public string? sunset { get; set; }
+        public float? temp { get; set; }
+        public string? timezone { get; set; }
+        public int? ts { get; set; }
+        public float? uv { get; set; }
+        public int? vis { get; set; }
+        public Weather? weather { get; set; }
+        public string? wind_cdir { get; set; }
+        public string? wind_cdir_full { get; set; }
+        public int? wind_dir { get; set; }
+        public float? wind_spd { get; set; }
+    }
+
+    public class Weather
+    {
+        public string? description { get; set; }
+        public int? code { get; set; }
+        public string? icon { get; set; }
+    }
+}

commit a323b5b0f82cb3c88492c1b115553f88b6e02cb6

Fixed regression from 9d58c8fe89995aea8b1764097f3cfe98b8723f45

app/services/WeatherService.cs
@@ -83,7 +83,7 @@ public class WeatherService {
     /// </summary>
     /// <param name="_city">Any city name should do, if none is given London, UK is assumed</param>
     /// <returns>an asynchronous result containing <see>WeatherData</see></returns>
-    public async Task<WeatherData> GetWeather(WeatherService.WeatherEndpoint _endpoint, string _city = "London", string _country = "UK") {
+    public async Task<WeatherData> GetWeather(string _city = "London", string _country = "UK") {
         // Get the API key from Weather Underground.
         string apiKey = sdb_app.Program.WeatherToken;
 
@@ -93,12 +93,24 @@ public class WeatherService {
         // Create a new HttpClient object.
         HttpClient client = new HttpClient();
 
+        void createQuery()
+        {
+            if (MyEndpoint.CountryQueryString == "")
+            {
+                _queryString = $"{MyEndpoint.Endpoint}{MyEndpoint.CityQueryString}{_city}&{MyEndpoint.AppID}{apiKey}";
+            }
+            else
+            {
+                _queryString = $"{MyEndpoint.Endpoint}{MyEndpoint.CityQueryString}{_city}&{MyEndpoint.CountryQueryString}{_country}&{MyEndpoint.AppID}{apiKey}";
+            }
+        }
+        createQuery();
         // Create the request before sending it to the server.
         Uri request = new Uri(_queryString);
-
+        // Debug output of the request
         if (sdb_app.Program.IsDebug)
         {
-            Program.discordClient.Logger.Log(LogLevel.Information, $"This is the api request: {request.ToString}", _endpoint);
+            Program.discordClient.Logger.Log(LogLevel.Information, $"This is the api request: {request.ToString}", MyEndpoint);
         }
 
         // Send the request

app/slashCommands/WeatherCommand.cs
@@ -22,7 +22,7 @@ public class WeatherCommandModule : ApplicationCommandModule {
         // set its endpoint to that of weatherbit.io
         service.MyEndpoint = service.WeatherEndpoints.Find(x => x.Name.Equals("WeatherBit"));
         // Get the weather for the location through our specific endpoint
-        WeatherData weather = await service.GetWeather(service.MyEndpoint, _city,_country);
+        WeatherData weather = await service.GetWeather(_city,_country);
         // Simplify data access
         var data = weather.data[0];
         // Display the weather information.